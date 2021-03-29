86b60f947b7a8f6da60f4b8e714b983aa5a13d9b
Vadim <vtimofeev223@gmail.com>
2021-03-29 17:51:55 +0300

    contact request changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,39 @@
 <template>
- <div class="home">
+  <div class="home">
     <div class="head d_flex">
       <h1 class="head__h1">Contacts manager</h1>
       <div class="icons d_flex">
-        <svg @click="getContacts" class="iconSvg" id="updateContacts" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M13.645 2.35C12.195 0.9 10.205 0 7.995 0C3.575 0 0.00500488 3.58 0.00500488 8C0.00500488 12.42 3.575 16 7.995 16C11.725 16 14.835 13.45 15.725 10H13.645C12.825 12.33 10.605 14 7.995 14C4.685 14 1.995 11.31 1.995 8C1.995 4.69 4.685 2 7.995 2C9.655 2 11.135 2.69 12.215 3.78L8.995 7H15.995V0L13.645 2.35Z" fill="black" fill-opacity="0.54"/>
+        <svg @click="getContacts" class="iconSvg" id="updateContacts" width="16" height="16" viewBox="0 0 16 16"
+             fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path
+              d="M13.645 2.35C12.195 0.9 10.205 0 7.995 0C3.575 0 0.00500488 3.58 0.00500488 8C0.00500488 12.42 3.575 16 7.995 16C11.725 16 14.835 13.45 15.725 10H13.645C12.825 12.33 10.605 14 7.995 14C4.685 14 1.995 11.31 1.995 8C1.995 4.69 4.685 2 7.995 2C9.655 2 11.135 2.69 12.215 3.78L8.995 7H15.995V0L13.645 2.35Z"
+              fill="black" fill-opacity="0.54"/>
         </svg>
-        <svg @click="output" class="iconSvg" width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" clip-rule="evenodd" d="M2 0H16C17.1 0 18 0.9 18 2V16C18 17.1 17.1 18 16 18H2C0.89 18 0 17.1 0 16V12H2V16H16V2H2V6H0V2C0 0.9 0.89 0 2 0ZM8.5 14L7.09 12.59L9.67 10H0V8H9.67L7.09 5.41L8.5 4L13.5 9L8.5 14Z" fill="black" fill-opacity="0.54"/>
+        <svg @click="output" class="iconSvg" width="18" height="18" viewBox="0 0 18 18" fill="none"
+             xmlns="http://www.w3.org/2000/svg">
+          <path fill-rule="evenodd" clip-rule="evenodd"
+                d="M2 0H16C17.1 0 18 0.9 18 2V16C18 17.1 17.1 18 16 18H2C0.89 18 0 17.1 0 16V12H2V16H16V2H2V6H0V2C0 0.9 0.89 0 2 0ZM8.5 14L7.09 12.59L9.67 10H0V8H9.67L7.09 5.41L8.5 4L13.5 9L8.5 14Z"
+                fill="black" fill-opacity="0.54"/>
         </svg>
       </div>
     </div>
     <div class="frame d_flex">
       <div class="category frame-border">
-        <div class="category__row" v-for="storage in contactStorages" :key="storage.id" @click="getContactsInfo(storage.Id)">
+        <div class="category__row" v-for="storage in contactStorages" :key="storage.id"
+             @click="getContactsInfo(storage.Id)">
           <h2 class="category__row-h2">{{ storage.Id }}</h2>
         </div>
       </div>
       <div class="contacts frame-border d_flex d-f_column">
         <div class="contact" v-for="user in users" :key="user.id" @click="getContactInfo(user)">
-          <h2 class="contact__userName">{{  user.FullName ? user.FullName: user.IdUser }}</h2>
+          <h2 class="contact__userName">{{ user.FullName ? user.FullName : user.IdUser }}</h2>
           <p class="contact__mail">{{ user.ViewEmail }}</p>
         </div>
       </div>
       <div class="contact-info frame-border">
         <div class="contact-info__row d_flex">
           <h2 class="row__title">Name</h2>
-          <p class="row__value">{{ currentUser.FullName ? currentUser.FullName: currentUser.IdUser}}</p>
+          <p class="row__value">{{ currentUser.FullName ? currentUser.FullName : currentUser.IdUser }}</p>
 
         </div>
         <div class="contact-info__row d_flex">
@@ -56,23 +63,23 @@
         </div>
       </div>
     </div>
- </div>
+  </div>
 </template>
 
 <script>
 import axios from "axios";
+
 export default {
   name: 'Home',
   data: () => ({
     contactStorages: [],
-    uuids: [],
+    uuids: {},
     users: [],
     currentStorage: '',
     currentUser: {},
     host: ''
   }),
-  components: {
-  },
+  components: {},
   mounted() {
     this.host = localStorage.getItem('host')
     if (localStorage.getItem('contactStorages')) {
@@ -81,44 +88,60 @@ export default {
     } else {
       this.getContacts()
     }
-
   },
   methods: {
-     getContacts() {
-        let bodyFormData = new FormData();
-        bodyFormData.append('Module', 'Contacts');
-        bodyFormData.append('Method', 'GetContactStorages');
-        const token = localStorage.getItem('token')
+    update() {
+
+    },
+    getContacts() {
+      let bodyFormData = new FormData();
+      bodyFormData.append('Module', 'Contacts');
+      bodyFormData.append('Method', 'GetContactStorages');
+      const token = localStorage.getItem('token')
       console.log(token)
-        axios({
-          url: this.host,
-          method: 'POST',
-          async: true,
-          dataType: 'json',
-          data: bodyFormData,
-          headers: {
-            "Content-Type": "multipart/form-data",
-            "Authorization": "Bearer " + token
-          }
-        })
-            .then(res => {
-                  if (JSON.stringify(this.contactStorages) !== JSON.stringify(res.data.Result)) {
-                    this.contactStorages = res.data.Result
-                    const contactStorages = JSON.stringify(res.data.Result)
-                    localStorage.setItem('contactStorages', contactStorages)
+      axios({
+        url: this.host,
+        method: 'POST',
+        async: true,
+        dataType: 'json',
+        data: bodyFormData,
+        headers: {
+          "Content-Type": "multipart/form-data",
+          "Authorization": "Bearer " + token
+        }
+      })
+          .then(res => {
+                if (JSON.stringify(this.contactStorages) !== JSON.stringify(res.data.Result)) {
+                  this.contactStorages = res.data.Result
+                  const contactStorages = JSON.stringify(res.data.Result)
+                  localStorage.setItem('contactStorages', contactStorages)
+                  let uuids = {}
+                  if (!localStorage.getItem('uuids')) {
+                    for (let i = 0; i < this.contactStorages.length; i++) {
+                      uuids[this.contactStorages[i].Id] = {}
+                    }
+                    localStorage.setItem('uuids', JSON.stringify(uuids))
+                  } else {
+                    uuids = JSON.parse(localStorage.getItem('uuids'))
+                  }
+                  const uuidsKeys = Object.keys(uuids)
+                  for (let i = 0; i < uuidsKeys.length; i++) {
+                    console.log(uuidsKeys[i])
+                    this.getContactsInfo(uuidsKeys[i])
                   }
                 }
-            )
-     },
-    getContactsInfo(storage) {
+              }
+          )
+    },
+    async getContactsInfo(storage) {
 
-       const storageArr = localStorage.getItem(`${storage}Arr`)
-       if (storageArr !== 'undefined') {
-         this.users = JSON.parse(storageArr)
-       } else {
-         localStorage.removeItem(storage)
-         this.users = []
-       }
+      const storageArr = localStorage.getItem(`${storage}Arr`)
+      if (storageArr !== 'undefined' && storageArr !== null) {
+        this.users = JSON.parse(storageArr)
+      } else {
+        localStorage.removeItem(storage)
+        this.users = []
+      }
       this.currentStorage = storage
       let bodyFormData = new FormData();
       bodyFormData.append('Module', 'Contacts');
@@ -139,24 +162,47 @@ export default {
         }
       })
           .then(res => {
+                const requestUuids = []
+                const contactsInfo = res.data.Result.Info
+                this.uuids = JSON.parse(localStorage.getItem('uuids'))
+
+                console.log(this.uuids[storage], storage)
                 if (localStorage.getItem(storage) !== String(res.data.Result.CTag) || !storageArr) {
-                  localStorage.setItem(storage, res.data.Result.CTag)
-                  const contactsInfo = res.data.Result.Info
-                  this.uuids = []
+                  let uuid = ''
                   for (let i = 0; i < contactsInfo.length; i++) {
-                    this.uuids.push(`"${contactsInfo[i].UUID}"`)
+                    uuid = contactsInfo[i].UUID
+                    requestUuids.push(`"${uuid}"`)
+                    this.uuids[storage][uuid] = contactsInfo[i].ETag
                   }
-                  this.GetContactsByUids(token)
+                  localStorage.setItem(storage, res.data.Result.CTag)
+                  console.log('kakogo hya')
+                } else {
+                  console.log('set storage')
+                  localStorage.setItem(storage, res.data.Result.CTag)
+                  let uuid = ''
+                  for (let i = 0; i < contactsInfo.length; i++) {
+                    uuid = contactsInfo[i].UUID
+                    console.log(this.uuids[storage][uuid], ' uid')
+                    console.log(contactsInfo[i].ETag, ' etag')
+                    if (this.uuids[storage][uuid] !== contactsInfo[i].ETag) {
+                      requestUuids.push(`"${uuid}"`)
+                      this.uuids[storage][uuid] = contactsInfo[i].ETag
+                    }
+                  }
+                }
+                localStorage.setItem('uuids', JSON.stringify(this.uuids))
+                if (requestUuids.length > 0) {
+                  this.GetContactsByUids(token, requestUuids)
                 }
               }
           )
     },
-    GetContactsByUids(token) {
+    GetContactsByUids(token, uuids) {
       let bodyFormData = new FormData();
       bodyFormData.append('Module', 'Contacts');
       bodyFormData.append('Method', 'GetContactsByUids');
       bodyFormData.append(
-          'Parameters', `{"Storage":"${this.currentStorage}","Uids":[${this.uuids}]}`
+          'Parameters', `{"Storage":"${this.currentStorage}","Uids":[${uuids}]}`
       )
 
       axios({
@@ -171,17 +217,35 @@ export default {
         }
       })
           .then(res => {
-            this.users = res.data.Result
-            if (this.users.length > 0) {
-              localStorage.setItem(`${this.currentStorage}Arr`, JSON.stringify(this.users))
+            let usersLocalStorage = localStorage.getItem(`${this.currentStorage}Arr`)
+
+            if (usersLocalStorage !== null) {
+              usersLocalStorage = JSON.parse(usersLocalStorage)
             }
+            let usersRes = res.data.Result
+            if (usersLocalStorage !== null) {
+              for (let i = 0; i < usersLocalStorage.length; i++) {
+                for (let j = 0; j < usersRes.length; j++) {
+                  if (usersLocalStorage[i]['UUID'] === usersRes[j]['UUID']) {
+                    usersLocalStorage[i] = usersRes[j]
+                    usersRes.splice(j, 1)
+                    break
+                  }
+                }
+              }
+              this.users = usersLocalStorage.concat(usersRes)
+            }
+            {
+              this.users = res.data.Result
+            }
+            localStorage.setItem(`${this.currentStorage}Arr`, JSON.stringify(this.users))
           })
     },
     getContactInfo(user) {
       this.currentUser = user
     },
     output() {
-       localStorage.clear()
+      localStorage.clear()
       this.$router.push('/')
     }
   }
@@ -197,20 +261,23 @@ export default {
   align-items: center;
   justify-content: space-between;
   border: 1px solid rgba(0, 0, 0, 0.12);
-  box-shadow:
-      0px 0px 2px rgba(0, 0, 0, 0.14),
-      0px 2px 2px rgba(0, 0, 0, 0.12),
-      0px 1px 3px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.14),
+  0px 2px 2px rgba(0, 0, 0, 0.12),
+  0px 1px 3px rgba(0, 0, 0, 0.2);
 }
+
 .head__h1 {
   padding-left: 16px;
 }
+
 .icons {
   align-items: center;
 }
+
 .icons svg {
   margin-right: 23px;
 }
+
 .iconSvg:hover {
   cursor: pointer;
   width: 20px;
@@ -220,45 +287,54 @@ export default {
 .frame-border {
   border-left: 1px solid rgba(0, 0, 0, 0.12);
   border-right: 1px solid rgba(0, 0, 0, 0.12);
-  box-shadow:
-      0px 0px 2px rgba(0, 0, 0, 0.14),
-      0px 2px 2px rgba(0, 0, 0, 0.12),
-      0px 1px 3px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.14),
+  0px 2px 2px rgba(0, 0, 0, 0.12),
+  0px 1px 3px rgba(0, 0, 0, 0.2);
 }
+
 .category {
   position: sticky;
   top: 7vh;
   height: 93vh;
 }
+
 .category__row {
   border-bottom: 1px solid rgba(0, 0, 0, 0.12);
 }
+
 .category__row:hover {
   background: #F4F4F4;
   cursor: pointer;
 }
+
 .category__row-h2 {
   padding: 12px 0 12px 16px;
   font-style: normal;
   font-weight: normal;
   font-size: 16px;
   line-height: 24px;
-  letter-spacing: 0.15px; color: rgba(0, 0, 0, 0.87);
+  letter-spacing: 0.15px;
+  color: rgba(0, 0, 0, 0.87);
   mix-blend-mode: normal;
 }
+
 .category {
   width: 26%;
 }
+
 .contact {
   border-bottom: 1px solid rgba(0, 0, 0, 0.12);
 }
+
 .contacts {
   width: 37%;
 }
+
 .contact:hover {
   background: #F4F4F4;
   cursor: pointer;
 }
+
 .contact__userName {
   font-style: normal;
   font-weight: normal;
@@ -269,6 +345,7 @@ export default {
   mix-blend-mode: normal;
   margin: 12px 0 3px 16px;
 }
+
 .contact__mail {
   font-style: normal;
   font-weight: normal;
@@ -279,19 +356,21 @@ export default {
   mix-blend-mode: normal;
   margin: 0 0 7px 16px;
 }
+
 .frame-border {
   border-left: 1px solid rgba(0, 0, 0, 0.12);
   border-right: 1px solid rgba(0, 0, 0, 0.12);
-  box-shadow:
-      0px 0px 2px rgba(0, 0, 0, 0.14),
-      0px 2px 2px rgba(0, 0, 0, 0.12),
-      0px 1px 3px rgba(0, 0, 0, 0.2);
+  box-shadow: 0px 0px 2px rgba(0, 0, 0, 0.14),
+  0px 2px 2px rgba(0, 0, 0, 0.12),
+  0px 1px 3px rgba(0, 0, 0, 0.2);
 }
+
 .contact-info {
   position: sticky;
   top: 7vh;
   height: 93vh;
 }
+
 .row__title {
   margin: 15px 0 8px 16px;
   font-style: normal;
@@ -303,6 +382,7 @@ export default {
   mix-blend-mode: normal;
   width: 35%;
 }
+
 .row__value {
   margin: 15px 0 8px 0;
   font-style: normal;
